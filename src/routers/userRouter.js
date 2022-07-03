a471a6d3f7b45ba80178be64c5a050177dccfe6f
hy-HA <heeyoung-ha@naver.com>
2022-07-03 13:11:30 +0900

    start edit profile

---
+++ src/routers/userRouter.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { getEdit, postEdit, remove, logout, see, startGithubLogin, finishGithubLogin } from "../controllers/userController";
+
+const userRouter = express.Router();
+
+userRouter.get("/logout", logout);
+userRouter.route("/edit").get(getEdit).post(postEdit);
+userRouter.get("/remove", remove);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get(":id", see);
+
+export default userRouter;
